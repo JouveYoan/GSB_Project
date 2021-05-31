5f1a4f509c3e44d8de28a8b35ef3a4580dd32e8d
Yoan Jouve <55242647+JouveYoan@users.noreply.github.com>
2021-05-31 15:44:53 +0200

    GSB dernière version

---
+++ PPEleger/index.php
@@ -0,0 +1,26 @@
+<doctype HTML!>
+    <html lang="fr">
+<html>
+<head>
+<meta charset="utf-8">
+<title> liste des visiteurs </title>
+<link href="CSS/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<center>
+<strong><H1>Qui êtes vous ? </H1></strong>
+                <p>
+                    <br>
+                    <td > <a href="connexion.php"> <img  src="https://image.flaticon.com/icons/png/512/44/44948.png" width=200px height=200px> </a> </td>
+                    <form method="post" action="php/connexion.php">  <input type="submit"  value='CONNEXION VISITEUR'> </form>
+                </p>
+                <p>
+                    <br>
+                    <td > <a href="admin.php"> <img  src="img/photoadmin.png"width=200px height=200px> </a> </td>
+                    <form method="post" action="php/admin.php">  <input type="submit"  value='CONNEXION ADMINISTRATEUR'></form>
+                </p>
+      </tr>
+</center>
+
+</body>
+</html>
